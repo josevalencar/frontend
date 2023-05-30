5e0ab7d63d423c1fceff607701a70906a1a38b58
GiuBontempo <giuliano.domiciano@gmail.com>
2023-05-30 15:46:59 -0300

    Separando as rotas em um arquivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import './global.css';
-import { Link, Route, Routes } from 'react-router-dom'
-import Colaboradores from './pages/colaboradores'
-import Home from './pages/home'
 import Nav from './components/nav'
-import Colaborador from './pages/colaborador'
+import Router from './router'
 
 function App() {
   return (
     <>
       <Nav />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="colaboradores">
-          <Route path="" element={<Colaboradores />} />
-          <Route path=":colaboradorId" element={<Colaborador />} />
-        </Route>
-      </Routes>
+      <Router />
     </>
   );
 }
