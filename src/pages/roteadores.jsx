ef0d8a0927e13f1f7335dd3bfab4e4bb90b6f8e7
GiuBontempo <giuliano.domiciano@gmail.com>
2023-05-31 17:20:49 -0300

    fix: consertando package lock

---
+++ src/pages/roteadores.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Roteadores = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Roteadores
---
+++ src/pages/setores.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Setores = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Setores
