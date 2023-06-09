1fa4240f4f8b526ae0dd6b4352d2181507ee04ba
marcelomaiaf <marcelo.filho@sou.inteli.edu.br>
2023-06-09 16:19:30 -0300

    help juba

--- src/components/modalTablets.jsx
+++ src/components/modalTablets.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/joy/Typography';
@@ -13,9 +14,46 @@ import AddIcon from '@mui/icons-material/Add';
 
 
 
-export default function ModalTablets(props) {
-  const { mode } = props;
+
+export default function ModalTablets(props, adicionaTablet) {
+  const { mode  } = props;
   const [open, setOpen] = React.useState(false);
+  const [tabletName, settabletName] = useState(null);
+  const [mac, setMac] = useState(null); 
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    fetch("https://2d1oh9-3000.csb.app/v1/esps/", {
+        method: "POST",
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            tabletName: tabletName,
+            mac: mac
+        })
+    })
+    .then(res => {
+        if(res.ok){
+            props.setSuccess(['cadastrado', true])
+        }
+        else{
+            props.setError(true);
+        }
+    })
+    .then(() => {
+        endFetch()
+    })
+    .catch((error) => console.log(error))
+}
+
+  const endFetch = () => {
+      settabletName(null);
+      setMac(null);
+      props.handleClose();
+      props.setGet(former => former + 1)
+  }
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -25,66 +63,92 @@ export default function ModalTablets(props) {
     setOpen(false);
   };
 
+  const handleChangeNome = (e) => {
+    settabletName(e.target.value);
+  }
+
+  const handleChangeMac = (e) => {
+    setMac(e.target.value);
+  }
+
+
   return (
     <div>
-      {mode === 'create'? 
-      <React.Fragment>
-        <Button
-         color="primary"
-         disabled={false}
-         onClick={handleClickOpen}
-         size="md"
-         variant="outlined"> <AddIcon/> Criar 
-        </Button>
-      </React.Fragment>:
-      <React.Fragment>
-        <IconButton onClick={handleClickOpen}>
-            <EditIcon sx={{ color: '#000000' }} />
-        </IconButton>
-      </React.Fragment>}
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>
-          {mode === 'create' ? 'Criar novo tablet' : 'Editar tablet'}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {mode === 'create'
-              ? 'Insira as informações do novo tablet:'
-              : (
-                <React.Fragment>
-                  Se você deseja editar o nome ou mac address do tablet, favor inserir abaixo:
-                  <Typography level="body4">
-                    Não é necessário o preenchimento de ambos os campos
-                  </Typography>
-                </React.Fragment>
-              )}  
-          </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="tabletName"
-              label="Nome do tablet"
-              type="text"
-              fullWidth
-              variant="standard"
-            />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="mac"
-            label="Mac"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-          <Button type='submit' onClick={handleClose}>
-            {mode === 'create' ? 'Criar' : 'Editar'}
+      <form 
+        name="create"
+        onSubmit={handleSubmit}
+      >
+        {mode === 'create'? 
+        <React.Fragment>
+          <Button
+          color="primary"
+          disabled={false}
+          onClick={handleClickOpen}
+          size="md"
+          variant="outlined"> <AddIcon/> Criar 
           </Button>
-        </DialogActions>
-      </Dialog>
+        </React.Fragment>:
+        <React.Fragment>
+          <IconButton onClick={handleClickOpen}>
+              <EditIcon sx={{ color: '#000000' }} />
+          </IconButton>
+        </React.Fragment>}
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>
+            {mode === 'create' ? 'Criar novo tablet' : 'Editar tablet'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {mode === 'create'
+                ? 'Insira as informações do novo tablet:'
+                : (
+                  <React.Fragment>
+                    Se você deseja editar o nome ou mac address do tablet, favor inserir abaixo:
+                    <Typography level="body4">
+                      Não é necessário o preenchimento de ambos os campos
+                    </Typography>
+                  </React.Fragment>
+                )}  
+            </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="tabletName"
+                name='tabletName'
+                label="Nome do tablet"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={handleChangeNome}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="mac"
+                name='mac'
+                label="Mac"
+                type="text"
+                fullWidth
+                variant="standard"
+                onChange={handleChangeMac}
+              />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancelar</Button>
+            {mode === 'create'? 
+            <React.Fragment>
+                <Button type='submit' onClick={handleClose} >
+                    Criar
+                </Button>
+            </React.Fragment>:
+            <React.Fragment>
+                <Button type='submit' onClick={handleClose} >
+                    Editar
+                </Button>
+            </React.Fragment>}
+          </DialogActions>
+        </Dialog>
+        </form>
     </div>
   );
 }
