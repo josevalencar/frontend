7bbc0e54c9d9668352d5e7c53c568aabf6409f73
GiuBontempo <giuliano.domiciano@gmail.com>
2023-06-20 09:36:40 -0300

    feat: search bar funcional roteadores

--- src/pages/roteadores.jsx
+++ src/pages/roteadores.jsx
@@ -3,14 +3,34 @@ import TableRoteador from '../components/roteador/tableRoteador';
 import ModalCriarRoteador from '../components/roteador/modalCreate';
 import SearchBar from '../components/roteador/searchbar';
 import CustomModalEdit from '../components/roteador/modalEdit';
+import removerAcentos from '../helpers/removerAcentos';
 
 
 const Roteadores = () => {
   const [roteadores, setRoteadores] = useState([]);
+  const [filter, updateFilter] = useState('');
+  const [displayedRouters, setDisplayedRouters] = useState(null);
+
+  useEffect(() => {
+    if (filter !== ''){
+      let filteredRouters = [];
+      roteadores.map((row) => {
+        if (row.name !== null){
+          if (removerAcentos(row.name.toLowerCase()).includes(filter) || row.name.includes(filter)){
+            filteredRouters.push(row)
+          }
+        }
+      })
+      console.log(filteredRouters);
+      setDisplayedRouters(filteredRouters);
+    } else{
+      setDisplayedRouters(roteadores);
+    }
+  }, [roteadores, filter])
 
   const adicionarRoteador = async (roteador) => {
     try {
-      const response = await fetch("https://2d1oh9-3000.csb.app/v1/esp-routers", {
+      const response = await fetch("https://sfqlqf-3000.csb.app/v1/esp-routers", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -26,7 +46,7 @@ const Roteadores = () => {
 
     setRoteadores([...roteadores, roteador]);
 
-    fetch("https://2d1oh9-3000.csb.app/v1/esp-routers")
+    fetch("https://sfqlqf-3000.csb.app/v1/esp-routers")
       .then((response) => response.json())
       .then(data => {
         const roteadoresUpdate = data.map(roteador => ({ ...roteador, macAddress: roteador.mac, routerName: roteador.name, routerID: roteador._id }));
@@ -40,7 +60,7 @@ const Roteadores = () => {
   };
 
   useEffect(() => {
-    fetch("https://2d1oh9-3000.csb.app/v1/esp-routers")
+    fetch("https://sfqlqf-3000.csb.app/v1/esp-routers")
       .then((response) => response.json())
       .then(data => {
         const roteadoresFetched = data.map(roteador => ({ ...roteador, macAddress: roteador.mac, routerName: roteador.name, routerID: roteador._id }));
@@ -53,12 +73,12 @@ const Roteadores = () => {
 
   const editarRoteador = async ({ routerName, macAddress, routerID }) => {
     console.log(routerID);
-    console.log(`https://2d1oh9-3000.csb.app/v1/esp-routers/` + routerID);
+    console.log(`https://sfqlqf-3000.csb.app/v1/esp-routers/` + routerID);
     console.log(`newName: ${routerName}, newMac: ${macAddress}`)
     console.log(roteadores);
 
     try {
-      const response = await fetch(`https://2d1oh9-3000.csb.app/v1/esp-routers/` + routerID, {
+      const response = await fetch(`https://sfqlqf-3000.csb.app/v1/esp-routers/` + routerID, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json"
@@ -91,7 +111,7 @@ const Roteadores = () => {
       console.error("Erro ao enviar a requisição para o backend", error);
     }
 
-    fetch("https://2d1oh9-3000.csb.app/v1/esp-routers")
+    fetch("https://sfqlqf-3000.csb.app/v1/esp-routers")
       .then((response) => response.json())
       .then(data => {
         const roteadoresNotEdited = data.map(roteador => ({ ...roteador, macAddress: roteador.mac, routerName: roteador.name, routerID: roteador._id }));
@@ -106,13 +126,13 @@ const Roteadores = () => {
     console.log(`Hello: ${routerID}`);
 
     try {
-      await fetch(`https://2d1oh9-3000.csb.app/v1/esp-routers/${routerID}`, { method: 'DELETE' });
+      await fetch(`https://sfqlqf-3000.csb.app/v1/esp-routers/${routerID}`, { method: 'DELETE' });
       console.log(`Roteador com ID ${routerID} deletado`);
     } catch (error) {
       console.error('Erro ao deletar roteador:', error);
     }
 
-    fetch("https://2d1oh9-3000.csb.app/v1/esp-routers")
+    fetch("https://sfqlqf-3000.csb.app/v1/esp-routers")
       .then((response) => response.json())
       .then(data => {
         const roteadoresNotDeleted = data.map(roteador => ({ ...roteador, macAddress: roteador.mac, routerName: roteador.name, routerID: roteador._id }));
@@ -131,14 +151,16 @@ const Roteadores = () => {
         <h1>Roteadores</h1>
       </div>
       <div style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: 110, alignItems: 'center', minHeight: '10vh' }} >
-        <SearchBar ></SearchBar>
+        <SearchBar updateFilter={updateFilter} type="roteador" />
         <ModalCriarRoteador adicionarRoteador={adicionarRoteador}></ModalCriarRoteador>
         <CustomModalEdit roteadores={roteadores} editarRoteador={editarRoteador}></CustomModalEdit>
 
 
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
-        <TableRoteador roteadores={roteadores} editarRoteador={editarRoteador} deletarRoteador={deletarRoteador}></TableRoteador>
+      {displayedRouters !== null && (
+          <TableRoteador onLoad={console.log(displayedRouters)} roteadores={displayedRouters} editarRoteador={editarRoteador} deletarRoteador={deletarRoteador} />
+        )}
       </div>
     </>
   )
