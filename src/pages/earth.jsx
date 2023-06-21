d5112a7a140916b0cced85ac1f1e8939977a6465
marcelomaiaf <marcelo.filho@sou.inteli.edu.br>
2023-06-21 11:02:20 -0300

    Melhorando earth

--- src/pages/earth.jsx
+++ src/pages/earth.jsx
@@ -64,7 +64,7 @@ export default function Viewer() {
         <Canvas camera={{ position: [-5, 0, 5], fov: 50 }} style={{ width: '600px', height: '600px' }}>
           <ambientLight intensity={0.5} />
           <Model position={[0, 0.25, 0]} />
-          {/* <Environment preset="city" /> */}
+          <Environment preset="city" /> 
           <ContactShadows frames={1} scale={5} position={[0, -1, 0]} far={1} blur={5} opacity={0.5} color="#204080" />
           <OrbitControls />
         </Canvas>
