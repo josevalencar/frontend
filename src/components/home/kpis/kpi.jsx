419589e4ac787ff7836b0b93cf879495a9b294f5
GiuBontempo <giuliano.domiciano@gmail.com>
2023-06-21 22:12:32 -0300

    feat: design home inicial

--- src/components/home/kpis/kpi.jsx
+++ src/components/home/kpis/kpi.jsx
@@ -4,15 +4,15 @@ const Kpi = (props) => {
   return (
     <div style={{width:"100%", height:"100%", display:"flex", flexDirection:"row", alignItems: "center", justifyContent: "center"}}>
         <div style={{width:"20%", height:"50%"}}>
-            <div style={{ width: "100%", height: "100%", borderRadius: "50%", backgroundColor: props.color, display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <div style={{ width: "120%", height: "200%", borderRadius: "50%", backgroundColor: props.color, display: "flex", alignItems: "center", justifyContent: "center" }}>
                 {props.icon}
             </div>
         </div>
-        <div style={{width:"80%", height:"100%", display:"flex", flexDirection:"column", alignItems: "center", justifyContent: "center", marginLeft:"10%"}}>
+        <div style={{width:"80%", height:"100%", display:"flex", flexDirection:"column", marginLeft:"5%"}}>
             <div style={{width:"100%", height:"500%", display:"flex", flexDirection:"row", alignItems: "flex-end", justifyContent: "flex-start"}}>
                 <h2 style={{margin:"0%"}}>{props.ammount}</h2>
             </div>
-            <div style={{width:"100%", height:"500%", display:"flex", flexDirection:"row", alignItems: "flex-start", justifyContent: "flex-start"}}>
+            <div style={{width:"80%", height:"500%", display:"flex", flexDirection:"row", alignItems: "flex-start", justifyContent: "flex-start", marginLeft:"3%"}}>
                 <p style={{margin:"0%"}}>{props.text}</p>
             </div>
         </div>
